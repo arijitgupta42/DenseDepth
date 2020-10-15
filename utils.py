07c1509d7b4227087b4f61211018493dafcc44bb
Arijit Gupta <arijitgupta2000@gmail.com>
2020-10-15 18:47:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -118,10 +118,10 @@ def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False):
         
         # Compute results
         true_y = depth[(i)*bs:(i+1)*bs,:,:]
-        pred_y = scale_up(2, predict(model, x/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
+        pred_y = scale_up(2, predict(model, x/255, minDepth=1, maxDepth=80, batch_size=bs)[:,:,:,0]) * 3.0
         
         # Test time augmentation: mirror image estimate
-        pred_y_flip = scale_up(2, predict(model, x[...,::-1,:]/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
+        pred_y_flip = scale_up(2, predict(model, x[...,::-1,:]/255, minDepth=1, maxDepth=80, batch_size=bs)[:,:,:,0]) * 3.0
 
         # Crop based on Eigen et al. crop
         true_y = true_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
